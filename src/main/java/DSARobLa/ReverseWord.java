edd59f9e2cbea3bb245ac93d464ae51f75ceae2e
Pettric H. Mamea <103038493+Beedrick@users.noreply.github.com>
2023-05-25 21:09:43 +1200

    Updated ReverseWord.java and StringStack.java to demonstrate Stack Data Structure

--- src/main/java/DSARobLa/ReverseWord.java
+++ src/main/java/DSARobLa/ReverseWord.java
@@ -9,5 +9,57 @@ package DSARobLa;
  * @author Gorilla Rig
  */
 public class ReverseWord {
+
+    private String input;
+    private String output;
+    
+    public ReverseWord(String in){
+        
+        this.input = in;
+        
+    }
+    
+    public void setInput(String input){
+        this.input = input;
+    }
+    
+    
+    //reverser method demonstrates some uses for Stack datastructure
+    //push method stacks the characters of the input String on top of 
+    //each other while the pop method removes all characters starting 
+    //from the top of the stack to the bottom while adding char to output String
+    public String reverser(){
+        
+        this.output = "";
+        StringStack stackX = new StringStack(input.length());
+        
+        
+        for(int index = 0; index < this.input.length(); ++index){
+            stackX.push(input.charAt(index));
+        }
+        
+        while(!stackX.isEmpty()){
+            this.output += stackX.pop();
+        }
+        
+        return this.output;
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        String input = "hello";
+        
+        ReverseWord rW = new ReverseWord(input);
+        
+        System.out.println(rW.reverser());
+        
+        input = "reverse";
+        rW.setInput(input);
+        
+        System.out.println(rW.reverser());
+        
+        
+    }
     
 }
--- src/main/java/DSARobLa/StringStack.java
+++ src/main/java/DSARobLa/StringStack.java
@@ -9,5 +9,41 @@ package DSARobLa;
  * @author Gorilla Rig
  */
 public class StringStack {
+
+    private int max;
+    private char[] a;
+    private int top;
+
+    public StringStack(int max) {
+        this.max = max;
+        this.a = new char[max];
+        this.top = -1;
+
+    }
+
+    public void push(char value) {
+        this.a[++top] = value;
+    }
+
+    public char pop() {
+        return this.a[top--];
+    }
+
+    public char peek() {
+        return this.a[top];
+    }
+
+    public void displayClear() {
+        while (top != -1) {
+            System.out.println(pop());
+
+        }
+    }
     
+    public boolean isEmpty(){
+        
+        return(this.top == -1);
+        
+    }
+
 }
